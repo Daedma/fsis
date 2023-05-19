1e4321d3ed2e7a3a53b761bdcba04fcb5a932f2a
Daedma <hdamir163@gmail.com>
2023-05-19 22:35:44 +0400

    Реализован класс Animation

---
+++ src/engine/animation/Animation.cpp
@@ -0,0 +1,70 @@
+#include "animation/Animation.hpp"
+#include <EASTL/algorithm.h>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+void Animation::play(bool bContinue)
+{
+	m_status = Status::PLAYING;
+	if (!bContinue)
+	{
+		m_curFrame = 0;
+	}
+}
+
+void Animation::stop()
+{
+	m_status = Status::STOPPED;
+	m_curFrame = 0;
+	m_elapsedTimeSinceFrameChange = 0.f;
+}
+
+void Animation::pause()
+{
+	m_status = Status::PAUSED;
+}
+
+void Animation::addNotify(const Notify& notify)
+{
+	if (eastl::none_of(m_notifies.begin(), m_notifies.end(), Notify::equal(notify)))
+	{
+		m_notifies.emplace_back(notify);
+	}
+}
+
+void Animation::setNotifyHandler(const eastl::string& notify, NotifyEventHandlerSignature handler)
+{
+	auto pos = eastl::find_if(m_notifies.begin(), m_notifies.end(), [&notify](const Notify& val) {
+		return notify == val.name;
+		});
+	EASTL_ASSERT(pos != m_notifies.end());
+	pos->handler = handler;
+}
+
+void Animation::tick(float deltaSeconds)
+{
+	switch (m_status)
+	{
+	case Status::PAUSED: case Status::STOPPED:
+		return;
+	}
+	m_elapsedTimeSinceFrameChange += deltaSeconds;
+	if (m_elapsedTimeSinceFrameChange > m_period)
+	{
+		m_curFrame = (++m_curFrame) % m_framesCount;
+		if (m_curFrame == 0 && !b_loop)
+		{
+			stop();
+		}
+		else
+		{
+			nextFrame();
+			handleNotifies();
+			m_elapsedTimeSinceFrameChange = 0.f;
+		}
+	}
+}
+
+void Animation::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(*m_sprite, states);
+}
---
+++ src/engine/animation/Animation.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <EASTL/unique_ptr.h>
+#include <EASTL/string.h>
+#include <EASTL/functional.h>
+#include <EASTL/vector.h>
+#include <EASTL/iterator.h>
+#include <EASTL/algorithm.h>
+#include "core/GameObject.hpp"
+#include "core/TransformTypes.hpp"
+
+class Animation : public sf::Drawable, public GameObject
+{
+public:
+	using NotifyEventHandlerSignature = eastl::function<void(float)>;
+
+	struct Notify
+	{
+		uint8_t frame;
+		eastl::string name;
+		NotifyEventHandlerSignature handler = nullptr;
+
+		static constexpr auto equal = [](const Notify& notify) {
+			return [&notify](const Notify& other) {
+				return notify.name == other.name;
+			};
+		};
+
+		static constexpr auto isMyFrame = [](uint8_t frame) {
+			return [frame](const Notify& notify) {return frame == notify.frame; };
+		};
+	};
+
+	Animation(World* world) : GameObject(world), m_sprite(new sf::Sprite()) {}
+
+	Animation() : m_sprite(new sf::Sprite()) {}
+
+	void setTexture(sf::Texture* texture)
+	{
+		m_texture = texture;
+		m_sprite->setTexture(*texture);
+	}
+
+	enum class Status
+	{
+		PLAYING, PAUSED, STOPPED
+	};
+
+	void play(bool bContinue = true);
+
+	void stop();
+
+	void pause();
+
+	Status getStatus() const { return m_status; }
+
+	void setHeight(float height)
+	{
+		EASTL_ASSERT(height != 0 && m_texture);
+		float ratio = SQRT_3 * height / m_initFrame.height;
+		m_sprite->setScale(ratio, ratio);
+	}
+
+	void setInitFrame(sf::IntRect frame)
+	{
+		m_initFrame = frame;
+	}
+
+	void setFramesCount(uint8_t framesCount)
+	{
+		m_framesCount = framesCount;
+	}
+
+	void setFrequency(float freq)
+	{
+		m_period = 1.f / freq;
+	}
+
+	float getDuration()
+	{
+		return m_period * m_framesCount;
+	}
+
+	void setLoop(bool loop) { b_loop = loop; }
+
+	uint8_t	getCurrentFrame() const { return m_curFrame; }
+
+	void addNotify(const Notify& notify);
+
+	void setNotifyHandler(const eastl::string& notify, NotifyEventHandlerSignature handler);
+
+	// remove notify on request
+
+	virtual void tick(float deltaSeconds) override;
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+	eastl::unique_ptr<sf::Sprite> m_sprite;
+
+	float m_period;
+
+	float m_elapsedTimeSinceFrameChange = 0.f;
+
+	uint8_t m_framesCount = 0;
+
+	uint8_t m_curFrame = 0;
+
+	sf::IntRect m_initFrame;
+
+	sf::Texture* m_texture = nullptr;
+
+	Status m_status = Status::STOPPED;
+
+	bool b_loop = true;
+
+	eastl::vector<Notify> m_notifies;
+
+private:
+	void nextFrame()
+	{
+		sf::IntRect frame = m_initFrame;
+		frame.left = m_initFrame.width * m_curFrame + m_initFrame.left;
+		m_sprite->setTextureRect(frame);
+	}
+
+	void handleNotifies()
+	{
+		eastl::vector<Notify> curFrameNotifies;
+		eastl::copy_if(m_notifies.begin(), m_notifies.end(),
+			eastl::back_inserter(curFrameNotifies), Notify::isMyFrame(m_curFrame));
+		for (auto& i : curFrameNotifies)
+		{
+			i.handler(m_period * m_curFrame);
+		}
+	}
+};
