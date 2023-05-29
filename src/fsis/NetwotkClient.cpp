34d3db75a5f85ca7e2ddeff3bc0fef92b58a9c81
bxlltrx <golandec.81@yandex.ru>
2023-05-29 18:46:01 +0000

    Replace NetwotkClient.cpp

--- src/fsis/NetwotkClient.cpp
+++ src/fsis/NetwotkClient.cpp
@@ -1,7 +1,10 @@
 #include "NetworkClient.hpp"
 #include <boost/json.hpp>
 #include <fstream>
+#include <cpr/cpr.h>
 
+
+  
 void NetworkClient::init(const std::string& filename)
 {
 	namespace json = boost::json;
@@ -12,24 +15,52 @@ void NetworkClient::init(const std::string& filename)
 	m_url = serverInfo.at("url").as_string();
 }
 
+
 NetworkClient::Status NetworkClient::auth(const std::string& login, const std::string& pass)
 {
 	// send login and pass to server ...
-	// get response ...
-	// if it's good
-	// m_userId = ...
-	return Status::SUCCESS;
-	// else
-	return Status::FAILURE;
+    cpr::Parameters parameters{{"username", login},
+         {"password", pass}};
+    auto response = cpr::Get(cpr::Url{ "http://localhost:8081/back/auth" }, parameters);
+    if (response.status_code == 200)
+    {
+        
+        m_userId = response.text;
+        return Status::SUCCESS;
+    }
+    else
+    {
+        
+        std::cout << "Ошибка авторизации. Код ошибки: " << response.status_code << std::endl;
+        return Status::FAILURE;
+    }
+
+   
 }
 
 bool NetworkClient::sendMatchStatsToServer(const MatchStats& stats)
 {
-	// send stats to server ...
-	// receive achievements ...
-	// if ok
-	// m_lastReceivedAchevement = ...
-	return true;
-	// else
-	return false;
+     cpr::Parameters parameters{{"score", stats.score},
+        {"kills", stats.kills},
+        {"steps", stats.steps},
+        {"sculcks", stats.sculcks},
+        {"waves", stats.waves},
+        {"duration", stats.duration},
+        {"specialAttackKills", stats.specialAttackKills}
+     };
+	auto response = cpr::Post(cpr::Url{ "http://localhost:8081/back/auth" }, parameters);
+
+    if (response.status_code == 200)
+    {
+        
+        m_lastReceivedAchievements = response.text;
+        return true;
+    }
+    else
+    {
+        
+        std::cout << "Ошибка авторизации. Код ошибки: " << response.status_code << std::endl;
+        return false;
+    }
 }
+    
