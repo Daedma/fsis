9e2443ddb171ac29f0ad523a918311c19c38a49a
Daedma <hdamir163@gmail.com>
2023-05-30 22:00:32 +0400

    Изменил интерфейс класса HUD

--- src/engine/gui/HUD.cpp
+++ src/engine/gui/HUD.cpp
@@ -1,12 +1,26 @@
 #include "gui/HUD.hpp"
 #include <SFML/Graphics/RenderStates.hpp>
+#include <boost/json.hpp>
+#include <fstream>
 
 void HUD::processInput(const sf::Event& event)
 {
 	tgui::GuiSFML::handleEvent(event);
 }
 
-void HUD::tick(float deltaSeconds)
+void HUD::loadForms(const char* filename)
 {
+	namespace json = boost::json;
 
+	std::ifstream ifs(filename);
+	json::array forms = json::parse(filename).as_object().at("forms").as_array();
+
+	for (const auto& i : forms)
+	{
+		tgui::Group::Ptr last = m_menuSet[i.as_object().at("name").as_string().c_str()];
+		last->loadWidgetsFromFile(i.as_object().at("file").as_string().c_str());
+		add(last);
+		last->setVisible(false);
+	}
 }
+
--- src/engine/gui/HUD.hpp
+++ src/engine/gui/HUD.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <TGUI/Backends/SFML.hpp>
+#include <TGUI/Widgets/Group.hpp>
+#include <EASTL/stack.h>
+#include <EASTL/string.h>
+#include <EASTL/hash_map.h>
 #include <core/InputEventListener.hpp>
 
 class HUD : public tgui::GuiSFML, public InputEventListener
@@ -10,5 +14,42 @@ public:
 
 	virtual void processInput(const sf::Event& event) override;
 
-	virtual void tick(float deltaSeconds);
+	tgui::Group::Ptr pushMenuItem(const eastl::string& menu)
+	{
+		if (!m_menuStack.empty())
+		{
+			m_menuStack.top()->setVisible(false);
+		}
+		m_menuStack.emplace(m_menuSet.at(menu));
+		m_menuStack.top()->setVisible(true);
+		return m_menuStack.top();
+	}
+
+	tgui::Group::Ptr popMenuItem()
+	{
+		if (m_menuStack.empty())
+		{
+			return nullptr;
+		}
+		tgui::Group::Ptr old = m_menuStack.top();
+		old->setVisible(false);
+		m_menuStack.pop();
+		if (!m_menuStack.empty())
+		{
+			m_menuStack.top()->setVisible(true);
+		}
+		return old;
+	}
+
+	tgui::Group::Ptr getCurrentMenuItem()
+	{
+		return m_menuStack.top();
+	}
+
+	void loadForms(const char* filename);
+
+private:
+	eastl::stack<tgui::Group::Ptr> m_menuStack;
+
+	eastl::hash_map<eastl::string, tgui::Group::Ptr> m_menuSet;
 };
