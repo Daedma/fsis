f0dec6b2e46bdbccd32198c4c4ba54b58166a358
Daedma <hdamir163@gmail.com>
2023-05-31 20:50:20 +0400

    Переместил файлы для форм

--- src/engine/gui/HUD.cpp
+++ src/engine/gui/HUD.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics/RenderStates.hpp>
 #include <boost/json.hpp>
 #include <fstream>
+#include <iostream>
 
 void HUD::processInput(const sf::Event& event)
 {
@@ -13,11 +14,12 @@ void HUD::loadForms(const char* filename)
 	namespace json = boost::json;
 
 	std::ifstream ifs(filename);
-	json::array forms = json::parse(filename).as_object().at("forms").as_array();
+	json::array forms = json::parse(ifs).as_object().at("forms").as_array();
 
 	for (const auto& i : forms)
 	{
 		tgui::Group::Ptr last = m_menuSet[i.as_object().at("name").as_string().c_str()];
+		last = tgui::Group::create();
 		last->loadWidgetsFromFile(i.as_object().at("file").as_string().c_str());
 		add(last);
 		last->setVisible(false);
--- src/fsis/ClientApplication.cpp
+++ src/fsis/ClientApplication.cpp
@@ -5,17 +5,22 @@
 #include "states/AuthState.hpp"
 #include "gui/HUD.hpp"
 #include "NetworkClient.hpp"
+#include <iostream>
 
 ClientApplication::ClientApplication()
 {
 	AssetManager::init("resources.ini");
+	std::cout << "Recources initialized\n";
 	AssetManager::initAnimGroups("animgroups.ini");
+	std::cout << "Animation groups initialized\n";
 
-	NetworkClient::init("client.ini");
+	NetworkClient::init("services.ini");
+	std::cout << "Network client initialized\n";
 
 	Canvas::init("FSIS");
 	Canvas::setHUD<HUD>();
 	Canvas::getHUD()->loadForms("forms.ini");
+	std::cout << "Forms are loaded\n";
 
 	m_curState = AuthState::getInstance();
 }
@@ -27,15 +32,12 @@ void ClientApplication::run()
 	{
 		m_curState = m_curState->nextState(); // Update state
 		sf::Event event;
-		switch (event.type)
+		while (Canvas::getWindow()->pollEvent(event))
 		{
-			while (Canvas::getWindow()->pollEvent(event))
+			InputEventListener::handleEvent(event);
+			if (event.type == sf::Event::Closed)
 			{
-				InputEventListener::handleEvent(event);
-				if (event.type == sf::Event::Closed)
-				{
-					Canvas::getWindow()->close();
-				}
+				Canvas::getWindow()->close();
 			}
 		}
 		m_curState->tick(ticks.restart().asSeconds());
